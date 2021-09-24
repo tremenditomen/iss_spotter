c0b4fd82dfa3d958bae95987041d51a7219ab337
Luis Andres Contreras <tremenditomen@gmail.com>
2021-09-24 07:59:59 -0600

    getting info from api

--- iss.js
+++ iss.js
@@ -4,9 +4,25 @@ const request = require('request')
 
 const fetchMyIp = ()=>{
    const argument = process.argv.slice(2).join("") 
-   request()
+   request('https://api.ipify.org?format=json',(error,response,body)=>{
+
+   const ipAdress = JSON.parese(body);
+
+   if (error){
+callback(error,null)
+return
+
+   }
+
+   if(response.statusCode!== 200){
+       const msg = `Status Code ${response.statuscode}when fetching Ip. response: ${body}`;
+       callback(Error(msg),null);
+       return
+   }
+   callback(null,ipAdress.ip)
+   });
 
 
-}
+};
 
 modules.export = {fetchMyIp}
