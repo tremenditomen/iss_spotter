cb758d978a06e755092d2c25022b98bb350b5500
Luis Andres Contreras <tremenditomen@gmail.com>
2021-11-10 11:06:30 -0700

    iss with promises

---
+++ iss_promisses.js
@@ -0,0 +1,29 @@
+const request = require("request-promise-native");
+
+const fetchMyIP = () => {
+  return request("https://api.ipify.org?format=json");
+};
+
+const fetchCoordsByIP = (data) => {
+  const ipAddress = JSON.parse(data).ip;
+  return request(`https://ipvigilante.com/json/${ipAddress}`);
+};
+
+const fetchISSFlyOverTimes = (coords) => {
+  const { latitude, longitude } = JSON.parse(coords).data;
+  return request(
+    `http://api.open-notify.org/iss-pass.json?lat=${latitude}&lon=${longitude}`
+  );
+};
+
+const nextISSTimesForMyLocation = () => {
+  return fetchMyIP()
+    .then(fetchCoordsByIP)
+    .then(fetchISSFlyOverTimes)
+    .then((data) => {
+      const { response } = JSON.parse(data);
+      return response;
+    });
+};
+
+module.exports = { nextISSTimesForMyLocation };
